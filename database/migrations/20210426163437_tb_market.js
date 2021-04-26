5acb92de5bd4790f6bb99d210058496b9e73e034
Box <saviomendesmiranda@gmail.com>
2021-04-26 16:54:39 -0300

    Create tb_market migration

--- database/migrations/20210426163437_tb_market.js
+++ database/migrations/20210426163437_tb_market.js
@@ -1,8 +1,28 @@
 
-exports.up = function(knex) {
-  
+const tableName = 'tb_market';
+
+exports.up = async function (knex) {
+    await knex.schema.createTable(tableName, function (table) {
+        table.increments('id').primary().notNullable();
+        table.string('business_name',150).notNullable();
+        table.string('cnpj', 14).notNullable().unique();
+        table.string('email', 150).notNullable().unique();
+        table.string('phone', 15).notNullable().unique();
+        table.string('password', 255).notNullable();
+        table.boolean('is_deleted').defaultTo(false).notNullable();
+        table.integer('id_address').notNullable().unsigned().references('id').inTable('tb_address').onDelete('CASCADE');
+        table.timestamps(false, true); //created_at/updated_at
+    });
+
+    await knex.raw(`
+        CREATE TRIGGER update_timestamp
+        BEFORE UPDATE
+        ON ${tableName}
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_timestamp();
+    `);
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTable(tableName);
 };
