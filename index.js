6f525604223c168f9563fac7e65e6c737f129199
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2021-04-26 16:40:37 -0300

    Novo index

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-const path = require('path')
-const Message = require('./utils/Message')
-const morgan = require('morgan')
-const express = require('express')
-const app = express()
-const router = require('./routes/routes')
-const helmet = require('helmet')
+require('dotenv').config();
 
-const cors = require('cors')
-app.use(cors())
+const Message = require('./utils/Message');
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(helmet())
-app.use(morgan('dev'))
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
-if (process.env.STORAGE_TYPE === 'local') {
-    app.use(
-        '/files',
-        express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
-    )
-}
+const app = express();
+const router = require('./routes/routes');
 
-// Raiz usará o o modulo router
-app.use('/', router)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
 
-Message.info('\nNão faça alterações no sistema!')
+// Raiz usará o módulo router
+app.use('/', router);
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server, {
-    allowEIO4: true,
-    cors: {
-        origin: 'http://localhost:3001'
-    }
-})
-const WebSockets = require('./utils/WebSockets.js')
-
-global.io = io.listen(server)
-global.io.on('connection', WebSockets.connection)
-
-server.listen(process.env.APP_PORT || 3000, error => {
-    Message.release(`\nAPI Versão: ${process.env.APP_VERSION}`)
-    Message.success(
-        `Servidor rodando na porta: ${process.env.APP_PORT || 3000}  ;^)\n`
-    )
-    if (error) Message.error('Servidor encerrado! >:^(\n')
-})
+app.listen(process.env.APP_PORT, (error) => {
+    Message.release('\nVersão: 0.0.1')
+    Message.success(`\nServidor rodando na porta ${process.env.APP_PORT}\n`);
+    if(error) Message.error('Servidor encerrado!\n');
+});
