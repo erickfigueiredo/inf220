bb46396bb36c91fc7779113844684d9da99b13bf
Box <saviomendesmiranda@gmail.com>
2021-04-26 17:04:04 -0300

    Create tb_order-product migration

--- database/migrations/20210426163535_tb_order_product.js
+++ database/migrations/20210426163535_tb_order_product.js
@@ -1,8 +1,28 @@
+const tableName = 'tb_order_product';
 
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+    await knex.schema.createTable(tableName, function (table) {
+        table.increments('id').primary().notNullable();
+        table.decimal('price', 8, 2).notNullable();
+        table.decimal('discount', 8, 2).notNullable();
+        table.integer('quantity').notNullable()
+        table.string('description', 255).notNullable();
+        table.boolean('is_deleted').defaultTo(false).notNullable();
+        table.integer('id_product').notNullable().unsigned().references('id').inTable('tb_product').onDelete('CASCADE');
+        table.integer('id_order').notNullable().unsigned().references('id').inTable('tb_order').onDelete('CASCADE');
+        table.timestamps(false, true);
+    });
+
+    await knex.raw(`
+        CREATE TRIGGER update_timestamp
+        BEFORE UPDATE
+        ON ${tableName}
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_timestamp();
+    `);
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTable(tableName);
 };
+
