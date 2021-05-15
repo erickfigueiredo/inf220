b4352a26617ee6f54a47b907782c25794ca4baab
Box <saviomendesmiranda@gmail.com>
2021-05-15 18:22:23 -0300

    Cart controller

---
+++ backend/controllers/CartController.js
@@ -0,0 +1,109 @@
+const Cart = require('../models/Cart');
+const Client = require('../models/Client');
+const Product = require('../models/Product');
+
+class CartController {
+    static async index(req, res) {
+        const id_user = req.params.id_user;
+
+        if (isNaN(parseInt(id_user)))
+            return res.status(400).send({ success: false, message: 'Id inválido' });
+
+        const user = await Client.findOne(id_user);
+        if (!user.success) return res.status(400).send({ success: false, message: 'Usuário não existe!' });
+
+        const cart = await Cart.findCartByClient(id_user, page);
+        return cart.success ? res.send(cart) : res.status(404).send(cart);
+    }
+
+    static async show(req, res) {
+        const id_item = req.params.id_item;
+
+        if (isNaN(parseInt(id_item)))
+            return res.status(400).send({ success: false, message: 'Id inválido' });
+
+        const cart = await Cart.findOne(id_item);
+        cart.success ? res.send(cart) : res.status(404).send(cart);
+    }
+
+    static async quantity(req, res) {
+        const id_user = req.params.id_user;
+
+        if (isNaN(parseInt(id_user)))
+            return res.status(400).send({ success: false, message: 'Id inválido' });
+
+        const user = await Client.findOne(id_user);
+        if (!user.success) return res.status(404).send('Usuário não existe!');
+
+        const cart = await Cart.findCartQuantity(id_user);
+        return cart.success ? res.send(cart) : res.status(404).send(cart);
+    }
+
+    static async create(req, res) {
+        const { id_user, id_product, quantity } = req.body
+
+        let data = {
+            id_user,
+            id_product,
+            quantity
+        }
+
+        const itemsInCart = await Cart.findCartByUser(id_user);
+        if (itemsInCart.success && itemsInCart.cart.data.length) {
+            for (let item of itemsInCart.cart.data) {
+                if (item['id_product'] == id_product && item['cart_quantity'] + quantity > item.product_quantity) return res.status(400).send({ success: false, message: 'Quantidade maior que a disponível!' });
+                else if (item['id_product'] == id_product) {
+                    data.quantity = item.cart_quantity + quantity;
+                    data.exists = true;
+                }
+            }
+        }
+
+        if (data.exists) {
+            const result = await Cart.updateByIdUserAndIdProduct(id_user, id_product, data.quantity);
+            return result.success ? res.send(result) : res.status(400).send(result);
+        } else {
+            const result = await Cart.create(data);
+            return result.success ? res.send(result) : res.status(400).send(result);
+        }
+    }
+
+    static async update(req, res) {
+
+        const { id_item, quantity } = req.body;
+        if (!quantity) {
+            const result = await this.delete(req, res);
+            return result.success ? res.send(result) : res.status(400).send(result);
+        }
+
+        const cartItem = await Cart.findOne(id_item);
+        if (!cartItem.success)
+            return res.status(400).send(cartItem);
+
+
+        const product = await Product.findOne(cartItem.item.id_product);
+        console.log(cartItem)
+        if (quantity > product.product.quantity)
+            return res.status(400).send({ success: false, message: 'Quantidade maior que a disponível!' });
+
+        const result = await Cart.updateByIdItem(id_item, { quantity });
+        return result.success ? res.send(result) : res.status(400).send(result);
+    }
+
+    static async delete(req, res) {
+        const id_item = req.params.id_item || req.body.id_item;
+
+        if (isNaN(parseInt(id_item)))
+            return res.status(400).send({ success: false, message: 'Id inválido' });
+
+        const item = await Cart.findOne(id_item);
+        if ((!item.success || (item.success && !Object.keys(item.item).length)))
+            return res.status(404).send({ success: false, message: 'Item não existe!' });
+
+
+        const result = await Cart.delete(id_item);
+        return result.success ? res.send(result) : res.status(400).send(result);
+    }
+}
+
+module.exports = CartController;
---
+++ backend/models/Cart.js
@@ -0,0 +1,102 @@
+const knex = require('../database/knex');
+const Message = require('../utils/Message');
+
+class Cart {
+
+    static async findOne(id) {
+        try {
+            const cart = await knex.select(['tb_cart.id AS id_item', 'tb_product.id AS id_product', 'tb_cart.quantity AS cart_quantity', 'tb_product.title', 'tb_product.price', 'tb_product.quantity AS product_quantity', 'tb_product.url_image', 'tb_product.key_image', 'tb_product.id_material', 'tb_product.id_discount'])
+                .from('tb_cart')
+                .where({ 'tb_cart.id': id })
+                .orderBy('tb_cart.id')
+                .innerJoin('tb_product', 'tb_product.id', 'tb_cart.id_product')
+
+            return cart[0] ? { success: true, item: cart[0] } : { success: false, message: 'Não foi possível recuperar o item / Item inexistente!' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Houve um erro ao recuperar o item!' };
+        }
+    }
+
+    static async findCartByUser(id_user, page) {
+        try {
+            const cart = await knex('tb_cart')
+                .select(['tb_cart.id AS id_item', 'tb_product.id AS id_product', 'tb_cart.quantity AS cart_quantity', 'tb_product.title', 'tb_product.price', 'tb_product.quantity AS product_quantity', 'tb_product.url_image', 'tb_product.key_image', 'tb_product.id_material', 'tb_product.id_discount'])
+                .where({ 'tb_cart.id_user': id_user })
+                .orderBy('tb_cart.id')
+                .innerJoin('tb_product', 'tb_product.id', 'tb_cart.id_product')
+
+            return cart ? { success: true, cart } : { success: false, message: 'Não foi possível recuperar o item / Itens inexistentes!' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Houve um erro ao recuperar o carrinho!' };
+        }
+    }
+
+    static async findCartQuantity(id_user) {
+        try {
+            const cart = await knex('tb_cart')
+                .sum({ total: 'quantity' })
+                .where({ 'id_user': id_user });
+            return cart[0] ? { success: true, items: parseInt(cart[0].total) } : { success: false, message: 'Não foi possível recuperar a quantidade do carrinho / Itens inexistente!' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Houve um erro ao recuperar o carrinho!' };
+        }
+    }
+
+    static async create(data) {
+        try {
+            const item = await knex.insert(data).table('tb_cart').returning('*');
+
+            return item[0] ? { success: true, item: item[0] } : { success: false, message: 'Não foi possível cadastrar o item!' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Falha ao inserir item!' };
+        }
+    }
+
+    static async updateByIdItem(id_item, data) {
+        try {
+            const item = await knex.update(data)
+                .table('tb_cart')
+                .where({ id: id_item })
+                .returning('*');
+
+            return item[0] ? { success: true, item: item[0] } : { success: false, message: 'Falha ao atulizar item! ' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Item não atualizado!' };
+        }
+    }
+
+    static async updateByIdUserAndIdProduct(id_user, id_product, quantity) {
+        try {
+            const item = await knex.update({ quantity })
+                .table('tb_cart')
+                .where({ id_user })
+                .andWhere({ id_product })
+                .returning('*');
+
+            return item[0] ? { success: true, item: item[0] } : { success: false, message: 'Falha ao atulizar item! ' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Item não atualizado!' };
+        }
+    }
+
+    static async delete(id) {
+        try {
+            await knex.delete()
+                .table('tb_cart')
+                .where({ id });
+
+            return { success: true, message: 'Item deletado!' };
+        } catch (error) {
+            Message.warning(error);
+            return { success: false, message: 'Item não deletado!' };
+        }
+    }
+}
+
+module.exports = Cart;
