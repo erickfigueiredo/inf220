0b56d812b0c96a4bf2c677a115c9b12a6158fc93
erickfigueiredo <ericklima816@gmail.com>
2021-05-16 16:30:08 -0300

    Rota de cliente

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const AddressController = require('../controllers/AddressController');
+const CartController = require('../controllers/CartController');
+const CategoryController = require('../controllers/CategoryController');
 const ClientController = require('../controllers/ClientController');
 const DeliverymanController = require('../controllers/DeliverymanController');
 const MarketplaceController = require('../controllers/MarketplaceController');
@@ -8,12 +10,29 @@ const ProductController = require('../controllers/ProductController');
 const WalletController = require('../controllers/WalletController');
 
 // Address routes
-
 router.get('/address/all/:id_user', AddressController.index);
 router.get('/address/:id', AddressController.show);
 router.post('/address', AddressController.create);
 router.put('/address', AddressController.update);
 router.delete('/address/:id', AddressController.delete);
+
+// Cart route
+router.get('/cart/all/:id_user', CartController.index);
+router.get('/cart/:id_item', CartController.show);
+router.get('/cart/quantity', CartController.quantity);
+router.post('/cart', CartController.create);
+router.update('/cart', CartController.update);
+router.delete('/cart', CartController.delete);
+
+// Category route
+router.get('/category/all', CategoryController.index);
+
+// Client route
+router.get('/client/index', ClientController.index);
+router.get('/client/:id', ClientController.show);
+router.post('/client', ClientController.create);
+router.put('/client', ClientController.update);
+router.delete('/client/:id', ClientController.delete);
 
 // Erro 404
 router.get('/bermuda_triangle', (req, res) => {
