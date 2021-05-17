221407c4acd3a3355fa14e0335bd9e78d19dc5b4
Box <saviomendesmiranda@gmail.com>
2021-05-17 16:04:37 -0300

    Login screen

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -91,6 +91,8 @@ router.get('/withdraw/all/:id_user', WalletWithdrawController.index);
 router.get('/withdraw/:id_withdraw', WalletWithdrawController.show);
 router.post('/withdraw', WalletWithdrawController.create);
 
+//router.post('/login', Client.login);
+
 // Erro 404
 router.get('/bermuda_triangle', (req, res) => {
     res.status(404).send('ERRO 404 | What??? Maybe you should not be here... ¯\\_(ツ)_/¯');
