f1a79bd31ea61b8a42ff584e44498b200f43c0a0
erickfigueiredo <ericklima816@gmail.com>
2021-05-17 18:28:25 -0300

    Limpeza do index

--- backend/index.js
+++ backend/index.js
@@ -16,55 +16,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
-app.get('/login', function(req, res) {
-    res.render('pages/login');
-});
-
-app.get('/register', function(req, res) {
-    res.render('pages/register');
-});
-
-app.get('/cart', function(req, res) {
-    var oferts = [
-        {name:'teste1', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste2', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste3', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste4', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste4', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste3', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste4', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste4', preco:'testePreco', desc:'testeDesc', image:'srcImage'}
-    ];
-
-    res.render('pages/cart', {
-        oferts: oferts
-    });
-});
-
-app.get('/market', function(req, res) {
-    res.render('pages/market');
-});
-
-app.get('/product', function(req, res) {
-    res.render('pages/product');
-});
-
-app.get('/', function(req, res) {
-
-    var oferts = [
-        {name:'teste1', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste2', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste3', preco:'testePreco', desc:'testeDesc', image:'srcImage'},
-        {name:'teste4', preco:'testePreco', desc:'testeDesc', image:'srcImage'}
-    ];
-
-    res.render('pages/home', {
-        oferts: oferts
-    });
-});
 // Raiz usará o módulo router
 app.use('/', router);
 
